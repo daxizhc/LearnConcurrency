0500b077dc66762de380c4b2cbf47d79f51c8fa3
张浩辰 <3130102485@zju.edu.cn>
2020-01-13 16:57:14 +0800

    init

---
+++ src/test/java/com/tony/learnconcurrency/LearnConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.learnconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
