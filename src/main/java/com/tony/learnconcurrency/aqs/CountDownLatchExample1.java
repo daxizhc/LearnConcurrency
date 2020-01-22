c30811212da262cdef19e0bfe41f6b168ec1b9e3
张浩辰 <3130102485@zju.edu.cn>
2020-01-22 20:46:44 +0800

    CountDownLatch, Lock

---
+++ src/main/java/com/tony/learnconcurrency/aqs/CountDownLatchExample1.java
@@ -0,0 +1,45 @@
+package com.tony.learnconcurrency.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class CountDownLatchExample1 {
+
+    private static int threadCount = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    log.error("error", e);
+                }finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        log.info("finish");
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        Thread.sleep(100);
+        log.info("{}", threadNum);
+        Thread.sleep(100);
+    }
+
+
+}
